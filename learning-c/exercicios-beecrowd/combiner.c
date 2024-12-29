1f09384293010b8824c8aefdf822ecb3e186cc3d
BrazAc <braz0502002@gmail.com>
2024-12-28 22:26:03 -0300

    making a v2 i hope work pseudocode version

--- learning-c/exercicios-beecrowd/combiner.c
+++ learning-c/exercicios-beecrowd/combiner.c
@@ -1,99 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    //Obter a quantidade de testes, guardar m N
-    int N;
-    scanf("%d", &N);
+    //Criar 2 vetores de char (50 espaços)
+    //Criar 1 vetor de char (100 espaços)
+    
+    //Receber strings, guardar nos dois vetores de 50 espaços
 
-    //Repetir N vezes
-    char primeiraStr[50], segundaStr[50], strFinal[100];
-    for (int j = 0; j < N; j++) {
-        //Obter primeira e segunda strings (50 characters)
-        //guardar em primeiraStr e segundaStr
-        scanf("%s %s", primeiraStr, segundaStr);
+    //Iinicializar contadorPosicionadorStringFinal com 0
+    //Inicializar contadorStrings com 0
 
-        //Identificar menor e maior string, guardar em menorStr e maiorStr
-        char menorStr[50], maiorStr[50];
-        int cont1 = 0, cont2 = 0, cont = 0;
+    //Repetir
+        //Verificar se o char atual primeira string é diferente de '/0'
+        //Se sim:
+            //char atual na posicao contadorStrings vai pra posicao contadorPosicionadorStringFinal na string final
+        //Se nao:
+            //Iniciar um novo loop que preenche a string final com os resto da segunda string
+            //Repetir enquanto char atual da segunda string != '/0'
+                //char na posicao contadorPosicionadorStringFinal da string final receber char atual da segunda string posicao = contadorStrings
+                //contadorPosicionadorStringFinal aumenta 1
+                //contadorStrings aumenta 1
+            //Encerrar loop
 
-        while (primeiraStr[cont1] != '\0') {
-            cont1 ++;
-        }
-        while (segundaStr[cont2] != '\0') {
-            cont2 ++;
-        }
+        //contadorPosicionadorStringFinal aumenta 1
 
-        int sizeMaiorString, sizeMenorString;
+        //Verificar se o char atual segunda string é diferente de '/0'
+        //Se sim:
+            //char atual na posicao contadorStrings vai pra posicao contadorPosicionadorStringFinal na string final
+        //Se nao:
+            //Iniciar um novo loop que preenche a string final com os resto da primeira string
+            //contadorStrings aumenta 1
+            //Repetir enquanto char atual da primeira string != '/0'
+                //char na posicao contadorPosicionadorStringFinal da string final receber char atual da primeira string posicao = contadorStrings
+                //contadorPosicionadorStringFinal aumenta 1
+                //contadorStrings aumenta 1
+            //Encerrar loop
 
-        if (cont1 > cont2) {
-            for (int j = 0; j <= cont1; j++) {
-                maiorStr[j] = primeiraStr[j];
-            }
-            for (int j = 0; j <= cont2; j++) {
-                menorStr[j] = segundaStr[j];
-            }
-
-            //Descrobrir sizeMaiorString
-            sizeMaiorString = cont1;
-            sizeMenorString = cont2;
-        }else{
-            for (int j = 0; j <= cont2; j++) {
-                menorStr[j] = primeiraStr[j];
-            }
-            for (int j = 0; j <= cont1; j++) {
-                maiorStr[j] = segundaStr[j];
-            }
-
-            //Descrobrir sizeMaiorString
-            sizeMaiorString = cont2;
-            sizeMenorString = cont1;
-        }
-
-        printf("%d\n", sizeMaiorString);
-
-        //Iterar de 0 ate (sizeMaiorString - 1) passo 2
-        int cont3 = 0, sentinel = 1, sinalStr, i = 0;
-        while (cont3 < sizeMaiorString) {
-            //Identificar quando e qual string acabou
-            if (primeiraStr[cont3] == '\0' || segundaStr[cont3] == '\0'){
-                sentinel = 0;
-                if (primeiraStr[cont3] == '\0'){
-                    sinalStr = 2;
-                }else if (segundaStr[cont3] == '\0'){
-                    sinalStr = 1;
-                }
-            }
-            //Concatenar characters em strFinal  
-            //Sequencia alternante
-            if (sentinel == 1) {
-                strFinal[i] = primeiraStr[cont3];
-                strFinal[i + 1] = segundaStr[cont3];
-                printf("%c ", primeiraStr[cont3]);
-                printf("%c ", segundaStr[cont3]);
-            //Sequencia direta
-            }else{
-                if (sinalStr == 2) {
-                    //Concatenar o resto de segundaStr em strFinal
-                    strFinal[cont3] = segundaStr[cont3];
-                    printf("%c ", strFinal[cont3]);
-                }else{
-                    //Concatenar o resto de primeiraStr em strFinal
-                    strFinal[cont3] = primeiraStr[cont3];
-                    printf("%c\n", strFinal[cont3]);
-                }
-            }
-            cont3 ++;
-            i += 2;
-        }
-        printf("\n");
-        for (int i = 0; i < cont3; i++) {
-            printf("%c", strFinal[i]);
-        }
-        printf("\n");
-    }
-        
-
-            
+        //contadorPosicionadorStringFinal aumenta 1
+        //contadorStrings aumenta 1
+    
+    //Mostrar stringFinal
 
     return 0;
 }
