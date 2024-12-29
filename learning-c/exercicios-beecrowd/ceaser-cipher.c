e4b3b6e0828b6883c13db8d449d10a670e93ca96
BrazAc <braz0502002@gmail.com>
2024-12-29 20:38:20 -0300

    final working code version

--- learning-c/exercicios-beecrowd/ceaser-cipher.c
+++ learning-c/exercicios-beecrowd/ceaser-cipher.c
@@ -2,46 +2,72 @@
 #include <stdlib.h>
 
 //Definir constante para quantidade de letras do alfabeto chamada TAM_ALFABETO
+#define TAM_ALFABETO 26
 
 int main(){
     //cadastrar alfabeto no vetor de chars vetAlfabeto
+    char vetAlfabeto[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     //Declarar cripPalavra, descripPalavra e cripPasso
+    char cripPalavra[51], descripPalavra[51];
+    int cripPasso, N; 
 
-    //Obter palavra criptografada, guardar em cripPalavra
-    //Obter passo da criptografia, guardar em cripPasso
+    //Obter quantidade de casos
+    scanf("%d", &N);
 
-    //Inicializar cont com 0
+    //Repetir N vezes
+    for (int i = 0; i < N; i++) {
+        //Obter palavra criptografada, guardar em cripPalavra
+        scanf("%s", cripPalavra);
+        //Obter passo da criptografia, guardar em cripPasso
+        scanf("%d", &cripPasso);
 
-    //Declaro posAlfabeto 
+        //Inicializar cont com 0
+        int cont = 0;
 
-    //Repetir enquanto cripPalavra[cont] != '\0'
-        //Inicializo posAlfabeto com 0
+        //Declarar posAlfabeto 
+        int posAlfabeto;
 
-        //Guardar letra atual em letraAtual
+        //Repetir enquanto cripPalavra[cont] != '\0'
+        while (cripPalavra[cont] != '\0') {
+            //Inicializar posAlfabeto com 0
+            posAlfabeto = 0;
 
-        //Repetir sempre
-            //se letraAtual != vetAlfabeto[posAlfabeto]
-                //aumentar contAlfabeto em 1
-            //caso contrario 
-                //encerrar loop
+            //Guardar letra atual em letraAtual
+            char letraAtual = cripPalavra[cont];
 
-        //Calcular a posicao da letra descriptografada 
-        //posDecrip = TAM_ALFABETO - posAlfabeto
+            //Repetir sempre
+            while (1) {
+                //se letraAtual != vetAlfabeto[posAlfabeto]
+                if (letraAtual != vetAlfabeto[posAlfabeto]) {
+                    //aumentar posAlfabeto em 1
+                    posAlfabeto ++;
+                //caso contrario
+                }else{ 
+                    //encerrar loop
+                    break;
+                }
+            }
 
-        //Se posDecrip for negativa tratar a posicao
-            //posDecrip = TAM_ALFABETO _ posAlfabeto
-        
-        //Atualizar palavra final
-        //descripPalavra[cont] = vetAlfabeto[posAlfabeto]
+            //Calcular a posicao da letra descriptografada 
+            int posDecrip = posAlfabeto - cripPasso;
 
-        //cont aumenta em 1
-    
-    //Especificar que descripPalavra finalizou
-    //descripPalavra[cont] = '\0'
+            //Se posDecrip for negativa tratar a posicao
+            if (posDecrip < 0) {
+                posDecrip = TAM_ALFABETO + posDecrip;
+            }
 
-    //Mostrar descripPalavra
+            //Atualizar palavra final
+            descripPalavra[cont] = vetAlfabeto[posDecrip];
 
+            //cont aumenta em 1
+            cont ++;
+        }
 
+        //Especificar que descripPalavra finalizou
+        descripPalavra[cont] = '\0';
 
+        //Mostrar descripPalavra
+        printf("%s\n", descripPalavra);
+    }
     return 0;
 }
