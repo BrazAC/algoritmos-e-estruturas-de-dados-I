2ded3e309f85818df015408308b0769498b2b00c
BrazAc <braz0502002@gmail.com>
2024-12-29 13:08:41 -0300

    adding the complete pseudocode

---
+++ learning-c/exercicios-beecrowd/ceaser-cipher.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Definir constante para quantidade de letras do alfabeto chamada TAM_ALFABETO
+
+int main(){
+    //cadastrar alfabeto no vetor de chars vetAlfabeto
+    //Declarar cripPalavra, descripPalavra e cripPasso
+
+    //Obter palavra criptografada, guardar em cripPalavra
+    //Obter passo da criptografia, guardar em cripPasso
+
+    //Inicializar cont com 0
+
+    //Declaro posAlfabeto 
+
+    //Repetir enquanto cripPalavra[cont] != '\0'
+        //Inicializo posAlfabeto com 0
+
+        //Guardar letra atual em letraAtual
+
+        //Repetir sempre
+            //se letraAtual != vetAlfabeto[posAlfabeto]
+                //aumentar contAlfabeto em 1
+            //caso contrario 
+                //encerrar loop
+
+        //Calcular a posicao da letra descriptografada 
+        //posDecrip = TAM_ALFABETO - posAlfabeto
+
+        //Se posDecrip for negativa tratar a posicao
+            //posDecrip = TAM_ALFABETO _ posAlfabeto
+        
+        //Atualizar palavra final
+        //descripPalavra[cont] = vetAlfabeto[posAlfabeto]
+
+        //cont aumenta em 1
+    
+    //Especificar que descripPalavra finalizou
+    //descripPalavra[cont] = '\0'
+
+    //Mostrar descripPalavra
+
+
+
+    return 0;
+}
